af4d098d1d733faa1419e947fd95368f3966b28f
Trevor Jones <jones1876@gmail.com>
2025-10-05 00:03:04 -0400

    moved old RotoSdk to its own library as we won't be needing it later

---
+++ src/RotoSDK/Common/Model/RotationModel.cs
@@ -0,0 +1,17 @@
+﻿namespace com.rotovr.sdk
+{
+    [Serializable]
+    class RotationModel
+    {
+        public RotationModel(float angle, float time, int direction)
+        {
+            Angle = angle;
+            Time = time;
+            Direction = direction;
+        }
+
+        public float Angle { get; }
+        public float Time { get; }
+        public int Direction { get; }
+    }
+}
---
+++ src/RotoSDK/Registration.cs
@@ -0,0 +1,7 @@
+﻿namespace RotoSDK
+{
+    public class Class1
+    {
+
+    }
+}
